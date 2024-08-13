d82830831657d7a2f7503dc41bd2d7d9a5cc2c01
magedo992 <ahmeaged900@gmail.com>
2024-08-13 19:50:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express=require('express');
+const app=express();
+const axios=require('axios');
+const cors=require('cors');
+const path=require('path');
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+})
+
+app.get('/weather/:city',async (req,res)=>{
+    const city=req.params.city;
+    
+    
+
+    const apiKey='632b29c888f900ae617f0a99e528606c';
+    const resopnse=await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+    //console.log(resopnse);
+  
+    res.json(resopnse.data)
+})
+
+app.listen(3000,(err)=>{
+if(err)
+    console.log(err,'error');
+
+console.log('hosted on port 3000');
+
+    
+})
